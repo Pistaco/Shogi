6cd7c1d9e79084e12e5a79fab3e55dab8e2ca617
ignacio <helopython123@gmail.com>
2021-01-24 12:28:06 -0300

    1.2.1

--- src/front-end/src/components/casilla.js
+++ src/front-end/src/components/casilla.js
@@ -2,35 +2,51 @@ import react, {useContext, useState} from "react";
 import {CoordenadaContext} from "./../contexts/contextCoordenada";
 import styled from "styled-components";
 
+const Pieza = styled.div`
+display: flex;
+justify-content: center;
+font-size: 3rem;
+
+`
 
 const Casilla = ({row, column}) => {
 
-
     const {handCoo, Tablero_matrix, hand_matrix} = useContext(CoordenadaContext);
     const [coordenada, _] = useState([row, column])
-    const [pieza, moverPieza] = useState(null);
+    const [pieza, HandPieza] = useState(null);
     const [color, HandColor] = useState(false);
 
-const CasillaS = styled.div`
-width: 6.5em;
-height: 6.5em;
-background-color: ${color ? "#EFE46B" : "#DCC27F"};
-border: 1px solid black;
-`;
+    const CasillaS = styled.div`
+    width: 6.5em;
+    height: 6.5em;
+    background-color: ${color ? "#EFE46B" : "#DCC27F"};
+    border: 1px solid black;
+    `;
+
+    const help_pieza = () => {
+        const [x, y] = coordenada;
+        const tpieza = Tablero_matrix[x - 1][y - 1]
+        HandPieza(tpieza);
+    }
+    
     const HandClick = () => {
         if (color === false) {
             HandColor(true);
+            hand_matrix([coordenada, "P"]);
         }
         else {
         HandColor(false);
+        hand_matrix([coordenada, null]);
         }
-        hand_matrix([coordenada, "P"])
+        help_pieza();
     }
 
 
     return (
         <CasillaS onClick={HandClick} >
-            {pieza}
+            <Pieza>
+               {pieza}
+            </Pieza>
         </CasillaS>
     )
 
